e6733fda14665ab49f15f5e90721fccecf1b7937
bowenzhao <bzhao19@ucsc.edu>
2023-11-30 17:34:53 -0500

    file upload 1

---
+++ Interactable/camDisplayText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camDisplayText : MonoBehaviour
+{
+
+    private Text displayText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        System.DateTime time = System.DateTime.Now;
+
+        displayText.text = time.ToString() + " M3/3";
+    }
+}
